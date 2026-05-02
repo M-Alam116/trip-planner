52cdb021d713f6de1b4fa67f562b8f62569aa8df
Muhammad Alam <malam53345@gmail.com>
2026-05-02 17:01:31 +0500

    issue fix in backend

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.http import JsonResponse
 from logs.views import calculate_logs
 
+def home(request):
+    return JsonResponse({"status": "Backend is Running", "version": "1.0.0"})
+
 urlpatterns = [
+    path('', home),
     path('api/logs/calculate/', calculate_logs),
 ]
